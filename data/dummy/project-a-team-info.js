cb3014e575c7caf297868905323fef7742710275
kongukjae <kongukjae@gmail.com>
2024-06-14 17:13:23 +0900

    팀정보 자료구조 더미 작성

---
+++ data/dummy/project-a-team-info.js
@@ -0,0 +1,22 @@
+const KDT_IaaS_Project_A_Team_Info = {
+  A_TEAM : {
+    number : 1,
+    pm : "",
+    members : []
+  },
+  B_TEAM : {
+    number : 2,
+    pm : "",
+    members : []
+  },
+  C_TEAM : {
+    number : 3,
+    pm : "",
+    members : []
+  },
+  D_TEAM : {
+    number : 4,
+    pm : "",
+    members : []
+  }
+}
