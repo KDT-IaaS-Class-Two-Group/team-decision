5c4bdcf9796e91540fd389540616f778e3b366a0
kongukjae <kongukjae@gmail.com>
2024-06-14 14:20:07 +0900

    feat : 구상용 데이터

---
+++ issue1-structure.js
@@ -0,0 +1,45 @@
+const studentList = [
+  "피카츄",
+  "이상해씨",
+  "파이리",
+  "꼬부기",
+  "버터플",
+  "구구",
+  "피죤투",
+  "찌리리공",
+  "또가스",
+  "고오스",
+  "망키",
+  "파라스",
+  "디그다",
+  "쥬쥬",
+  "고지",
+  "슬리프",
+  "고우스트",
+  "켄타로스",
+  "파오리",
+  "깜지곰"
+];
+
+const KDT_IaaS_Project_A_Team_Info = {
+  A_TEAM : {
+    number : 1,
+    pm : "",
+    members : []
+  },
+  B_TEAM : {
+    number : 2,
+    pm : "",
+    members : []
+  },
+  C_TEAM : {
+    number : 3,
+    pm : "",
+    members : []
+  },
+  D_TEAM : {
+    number : 4,
+    pm : "",
+    members : []
+  }
+}
